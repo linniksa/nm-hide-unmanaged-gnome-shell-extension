50fa0b118aceef2da41a73905c3a1dd59590ce6d
xKippi <daniel.kipp@snorre.com>
2021-11-13 01:16:27 +0100

    Added support for change of device state

--- extension.js
+++ extension.js
@@ -1,4 +1,6 @@
 
+let handlers;
+let nmApplet;
 let origDeviceAdded;
 let NetworkManager;
 try {
@@ -7,12 +9,24 @@ try {
     NetworkManager = imports.gi.NetworkManager;
 }
 const Network = imports.ui.status.network;
+const Panel = imports.ui.panel;
+const Main = imports.ui.main;
 
 
 function enable() {
     origDeviceAdded = Network.NMApplet.prototype._deviceAdded;
+    nmApplet = Main.panel.statusArea["aggregateMenu"]._network;
+    handlers = {};
 
     let decoratedFunction = function (client, device, skipSyncDeviceNames) {
+        if(!(device in handlers)) {
+            handlers[device] = device.connect("state-changed", function(new_state, old_state, reason) {
+                if(new_state !== old_state) {
+                    nmApplet._readDevices();
+                }
+            });
+        }
+
         if (device.state === NetworkManager.DeviceState.UNMANAGED) {
             return;
         }
